3bbcaa4154101c96e7b9707f62f89f4375a54d3a
Andrea Guido <andrea.guido.research@gmail.com>
2022-10-07 14:02:45 +0200

    Update Exercise_Numpy.py

--- Exercise_Numpy.py
+++ Exercise_Numpy.py
@@ -12,3 +12,15 @@ NY = data[:,1]
 BO = data[:,2]
 CS = data[:,3]
 YR= data[:,0]
+
+## Goals:
+
+# 1 create a function that prints summary statistics (median, average and SD) of Runs scored of all teams 
+
+# 2 now include in the function the possibility to select a range of years and provide same statistics
+
+# 3 create a function to define who did more runs between two given teams
+
+# 4 create a function to compute moving averages over the time series
+
+# 5 t-test between two teams run scores
